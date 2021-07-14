9ed9062459b4550c1b18271dc720a034c9248f03
CEB5ACE3 <87115366+sisula@users.noreply.github.com>
2021-07-14 15:51:53 +0530

    FIX WHATSASENA

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *WhatsAlexa*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *WhatsAlexa*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *WhatsAlexa*:  ```" + msg + "```"
 }
 
 
