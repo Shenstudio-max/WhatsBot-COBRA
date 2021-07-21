8ab45b71a58ce80c021c044b93b617deb68f79f7
CEB5ACE3 <87115366+sisula@users.noreply.github.com>
2021-07-21 16:03:02 +0530

    Create tktok.js

---
+++ plugins/tktok.js
@@ -0,0 +1,23 @@
+
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+const Asena = require('../events');
+const Config = require('../config');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+
+
+if (Config.WORKTYPE == 'private') {
+  Asena.addCommand({pattern: 'tktok ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid);
+
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/tiktokwm?apikey=a776a857c5afc5855e0eb023&url=${match[1]}`, { responseType: 'arraybuffer' })
+
+   await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.video, {mimetype: Mimetype.jpg, caption: 'Copyright © 2021 | WhatsAlexa'})
+
+    }));
+}
