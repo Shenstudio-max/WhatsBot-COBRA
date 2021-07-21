5a9f2abed9c9acdf57d11f008a036e72a69f4c10
CEB5ACE3 <87115366+sisula@users.noreply.github.com>
2021-07-21 15:58:23 +0530

    Create ad3.js

---
+++ plugins/ad3.js
@@ -0,0 +1,121 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+const Asena = require('../events');
+const Config = require('../config');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('ttp');
+
+if (Config.WORKTYPE == 'private') {
+   Asena.addCommand({pattern: 'wetglass ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/ephoto1/wetglass?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+   Asena.addCommand({pattern: 'codw ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+  var topText, bottomText;
+    if (match[1].includes('/')) {
+        var split = match[1].split('/');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/ephoto2/codwarzone?apikey=a776a857c5afc5855e0eb023&text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+   Asena.addCommand({pattern: 'freefire ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/ephoto1/freefire?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+      Asena.addCommand({pattern: 'hacker ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/ephoto1/anonymhacker?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+   Asena.addCommand({pattern: 'bokeh ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/textprome/bokeh?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+      Asena.addCommand({pattern: 'shadow ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/photooxy1/shadow?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+      Asena.addCommand({pattern: 'burnp ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/photooxy1/burnpaper?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+   Asena.addCommand({pattern: 'love ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/photooxy1/love?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+      Asena.addCommand({pattern: 'lovew ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/photooxy1/woodheart?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+   
+   Asena.addCommand({pattern: 'dotavatar ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,NEED_WORD);
+  
+    var webimage = await axios.get(`http://lolhuman.herokuapp.com/api/ephoto1/avatardota?apikey=a776a857c5afc5855e0eb023&text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAlexa'})
+
+    }));
+}
