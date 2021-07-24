627a80af20f02a5fa8eb8539f9c2e485baeab6d4
CEB5ACE3 <87115366+sisula@users.noreply.github.com>
2021-07-24 17:36:13 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -91,12 +89,12 @@ if (config.LANG == 'ML') {
     dlang_input = 'പ്രോസസ്സ് ചെയ്ത വാചകം:'
 }
 if (config.LANG == 'HI') {
-    dlang_dsc = 'Guess the language of the replied message.'
-    closer_res = 'Closest Result:'
-    dlang_lang = 'Language:'
-    dlang_similarity = 'Similarity:'
-    dlang_other = 'Other Languages'
-    dlang_input = 'Processed Text:'
+    dlang_dsc = 'उत्तर दिए गए संदेश की भाषा का अनुमान लगाएं'
+    closer_res = 'निकटतम परिणाम:'
+    dlang_lang = 'जुबान:'
+    dlang_similarity = 'समानता:'
+    dlang_other = 'अन्य भाषाएँ'
+    dlang_input = 'संसाधित पाठ:'
 }
 if (config.LANG == 'ES') {
     dlang_dsc = 'Adivina el idioma del mensaje respondido.'
@@ -176,11 +174,11 @@ if (config.WORKTYPE == 'private') {
         succ_off = 'Antilink Uğurla Bağlandı!'
     }
     if (config.LANG == 'HI') {
-        l_dsc = 'Activates the Antilink tool.'
-        alr_on = 'Antilink is already open!'
-        alr_off = 'Antilink is currently closed!'
-        succ_on = 'Antilink Opened Successfully!'
-        succ_off = 'Antilink Closed Successfully!'
+        l_dsc = 'एंटीलिंक टूल को सक्रिय करता है।'
+        alr_on = 'एंटीलिंक पहले से ही खुला है!'
+        alr_off = 'एंटीलिंक वर्तमान में बंद है!'
+        succ_on = 'एंटीलिंक सफलतापूर्वक खोला गया!'
+        succ_off = 'एंटीलिंक सफलतापूर्वक बंद!'
     }
     if (config.LANG == 'ML') {
         l_dsc = 'ആന്റിലിങ്ക് ഉപകരണം സജീവമാക്കുന്നു.'
@@ -273,11 +271,11 @@ if (config.WORKTYPE == 'private') {
         succ_off_bio = 'Autobio Uğurla Bağlandı!'
     }
     if (config.LANG == 'HI') {
-        auto_dsc = 'Add live clock to your bio!'
-        alr_on_bio = 'Autobio is already open!'
-        alr_off_bio = 'Autobio is currently closed!'
-        succ_on_bio = 'Autobio Opened Successfully!'
-        succ_off_bio = 'Autobio Closed Successfully!'
+        auto_dsc = 'अपने बायो में लाइव घड़ी जोड़ें!'
+        alr_on_bio = 'Autobio पहले से ही खुला है!'
+        alr_off_bio = 'Autobio वर्तमान में बंद है!'
+        succ_on_bio = 'Autobio सफलतापूर्वक खोला गया!'
+        succ_off_bio = 'Autobio सफलतापूर्वक बंद!'
     }
     if (config.LANG == 'ML') {
         auto_dsc = 'നിങ്ങളുടെ ബയോയിലേക്ക് തത്സമയ ക്ലോക്ക് ചേർക്കുക!'
